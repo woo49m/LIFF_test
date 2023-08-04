0090914561c228c05bbca53224c548dd9d6aa4d7
Ian Hsu <woo49m@gmail.com>
2023-08-04 17:21:17 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -56,6 +56,39 @@ const sendMessage = () => {
       });
   }
 };
+
+const share = () => {
+  liff
+    .shareTargetPicker([
+      {
+        type: "text",
+        text: "Hello, World!",
+      },
+    ])
+    .then(function (res) {
+      if (res) {
+        // succeeded in sending a message through TargetPicker
+        console.log(`[${res.status}] Message sent!`);
+      } else {
+        const [majorVer, minorVer] = (liff.getLineVersion() || "").split(".");
+        if (parseInt(majorVer) == 10 && parseInt(minorVer) < 11) {
+          // LINE 10.3.0 - 10.10.0
+          // Old LINE will access here regardless of user's action
+          console.log(
+            "TargetPicker was opened at least. Whether succeeded to send message is unclear"
+          );
+        } else {
+          // LINE 10.11.0 -
+          // sending message canceled
+          console.log("TargetPicker was closed!");
+        }
+      }
+    })
+    .catch(function (error) {
+      // something went wrong before sending a message
+      console.log("something wrong happen");
+    });
+};
 </script>
 
 <template>
@@ -66,5 +99,6 @@ const sendMessage = () => {
       <div>{{ name }}</div>
     </div>
     <button @click="sendMessage">回傳訊息</button>
+    <button @click="share">分享</button>
   </div>
 </template>
